b40accf60cfc53a1a1aca062425248d9dc4cf84c
Chris Kinsman <chris@kinsman.net>
2019-05-22 07:58:50 -0700

    Add multi arch container builds

--- 6.build-container.sh
+++
@@ -1,4 +0,0 @@
-#! /bin/bash
-docker login
-docker build -t chriskinsman/rpi-tilloo:1 .
-docker push chriskinsman/rpi-tilloo:1
---
+++ 7.build-container-arm32v6.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+docker login
+docker build -t chriskinsman/rpi-tilloo:latest-arm32v6 .
+docker push chriskinsman/rpi-tilloo:latest-arm32v6
+
+docker manifest push --purge chriskinsman/rpi-tilloo:latest
+docker manifest create chriskinsman/rpi-tilloo:latest chriskinsman/rpi-tilloo:latest-amd64 chriskinsman/rpi-tilloo:latest-arm32v6
+docker manifest annotate chriskinsman/rpi-tilloo:latest chriskinsman/rpi-tilloo:latest-arm32v6 --os linux --arch arm
+docker manifest push chriskinsman/rpi-tilloo:latest
